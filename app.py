849be0fa41f6b5ef7507fa1dfd5307361227db44
hemantsirsat <hemantsirsat40@gmail.com>
2020-11-08 20:02:21 +0530

    Done with results. Styling Remaining

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from scripts.recommend import Recommend
+import sys
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        movie_user_like = request.form['movie_user_like']
+        session['movie'] = movie_user_like
+        return redirect(url_for('movie'))
+    return render_template("home.html")
+
+@app.route('/similar_movie',methods=['POST','GET'])
+def movie():
+    similar_movies = Recommend
+    names = similar_movies.show_similar_movies(session['movie'])
+    print(names)
+    return render_template('movie.html',movies = similar_movies.show_similar_movies(session['movie']),myMovie=session['movie'])
+
+if __name__=='__main__':
+    app.secret_key = 'highlyconfidential'
+    app.run(debug=True,port=5001)
---
+++ scripts/recommend.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+class Recommend:
+    
+    def show_similar_movies(movie_user_likes):
+        def get_title_from_index(index):
+            return df[df.index == index]["title"].values[0]
+
+        def get_index_from_title(title):
+            return df[df.title == title]["index"].values[0]
+
+        df = pd.read_csv('/home/hemant/Desktop_Custom/Online_Movie_Recomendation/scripts/movie_dataset.csv')
+
+        features = ['keywords','cast','genres','director']
+
+        def combine_features(row):
+            return row['keywords']+" "+row['cast']+" "+row['genres']+" "+row['director']
+
+        for feature in features:
+            df[feature] = df[feature].fillna('')
+            
+        df["combined_features"] = df.apply(combine_features,axis=1)
+        df.iloc[0].combined_features
+
+        cv = CountVectorizer()
+        count_matrix = cv.fit_transform(df['combined_features'])
+        cosine_sim= cosine_similarity(count_matrix)
+
+        movie_index = get_index_from_title(movie_user_likes)
+        similar_movies = list(enumerate(cosine_sim[movie_index]))
+        sorted_similar_movies = sorted(similar_movies,key=lambda x:x[1],reverse=True)[1:]
+
+        i=0
+        result = []
+        for element in sorted_similar_movies:
+            result.append((get_title_from_index(element[0])))
+            i=i+1
+            if i>5:
+                break
+        return [results for results in result] 
